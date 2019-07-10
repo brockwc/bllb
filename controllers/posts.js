e2182b89fc46cf4d4ae20156dbcfbd704f4845b3
Brock Whitbread <brock.whitbread@gmail.com>
2019-07-09 17:07:14 -0700

    Basic boilerplate for blog by Brock

---
+++ controllers/posts.js
@@ -0,0 +1,20 @@
+const Post = require('../models/post');
+
+module.exports = {
+  getAllPosts,
+  create
+};
+
+function getAllPosts(req, res) {
+  Post.find({}, function(err, posts) {
+    if (err) console.log(err);
+    res.status(200).json(posts);
+  });
+}
+
+function create(req, res) {
+  console.log('Hit create')
+  Post.create(req.body, function(err, post) {
+    console.log(post);
+  })
+}
--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import PostContainer from './components/PostContainer';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    posts: [],
+    newPost: {
+      title: "",
+      author: "",
+      body: ""
+    }
+  }
+
+  async componentDidMount() {
+    let posts = await fetch('/api/posts').then(res => res.json());
+    this.setState({ posts });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    fetch('/api/posts', 
+    {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(this.state.newPost)
+    })
+  }
+
+  handleChange = (e) => {
+    let newPost = { ...this.state.newPost }
+    newPost[e.target.name] = e.target.value;
+
+    this.setState({
+      newPost
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Bob Loblaw's Law Blog</h1>
+
+          <form onSubmit={this.handleSubmit}>
+            <div className="field-wrapper">
+              <label>Title</label>
+              <input 
+                name="title" 
+                type="text" 
+                onChange={ this.handleChange }
+                value={ this.state.newPost.title } />
+            </div>
+            <div className="field-wrapper">
+              <label>Author</label>
+              <input 
+                name="author" 
+                type="text" 
+                onChange={ this.handleChange }
+                value={ this.state.newPost.author } />
+            </div>
+            <div className="field-wrapper">
+              <label>Body</label>
+              <input 
+                name="body" 
+                type="text" 
+                onChange={ this.handleChange }
+                value={ this.state.newPost.body } />
+            </div>
+            
+            <input type="submit" value="Submit" />
+          </form>
+        </header>
+
+        { 
+          this.state.posts.length
+          ? <PostContainer posts={this.state.posts} />
+          : <h3 style={{ textAlign: 'center' }}>Loading...</h3>
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
